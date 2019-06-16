dd7011379cd3e8286d123772edff73d459592253
MarcoMiranda05 <marmirand@hotmail.com>
2019-06-16 17:30:52 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Your code here
+function mapToNegativize(array) {
+  let returnArray = [];
+  for (let i = 0; i < array.length; i++) {
+    returnArray.push(-array[i]);
+  }
+  return returnArray;
+}
+
+function mapToNoChange(array) {
+  let returnArray = [];
+  for (let i = 0; i < array.length; i++) {
+    returnArray.push(array[i]);
+  }
+  return returnArray;
+}
+
+function mapToDouble(array) {
+  let returnArray = [];
+  for (let i = 0; i < array.length; i++) {
+    returnArray.push(array[i] * 2);
+  }
+  return returnArray;
+}
+
+function mapToSquare(array) {
+  let returnArray = [];
+  for (let i = 0; i < array.length; i++) {
+    returnArray.push(array[i] * array[i]);
+  }
+  return returnArray;
+}
+
+function reduceToTotal(array, startingPoint = 0) {
+  let returnValue = startingPoint;
+  for (let i = 0; i < array.length; i++) {
+    returnValue += array[i];
+  }
+  return returnValue;
+}
+
+function reduceToAllTrue(array) {
+  let returnValue = true;
+  for (let i = 0; i < array.length; i++) {
+    if (!array[i]) {
+      returnValue = false;
+    }
+  }
+  return returnValue;
+}
+
+function reduceToAnyTrue(array) {
+  let returnValue = false;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i]) {
+      returnValue = true;
+    }
+  }
+  return returnValue;
+}
